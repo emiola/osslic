87c259c0550133c5c380a58eca09fb57cab4da88
AF <1955914-emiola@users.noreply.gitlab.com>
2021-10-07 01:24:46 -0500

    Refactoring

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
 import { argv } from 'node:process'
-import * as path from 'node:path'
-import { writeFile } from 'node:fs/promises'
-import { json2tsv } from 'tsv-json'
-import { getJsonFileContent } from './src/fs-helper.js'
-
-const JSON_EXT = '.json'
-const TSV_EXT = '.tsv'
+import { getJsonFileContent, exportJsonArrayToTsv } from './src/fs-helper.js'
 
 const filename = argv[2]
-const basename = path.basename(filename, JSON_EXT)
 
 const jsonContent = await getJsonFileContent(filename)
 const jsonSections = JSON.parse(jsonContent)
@@ -24,10 +17,4 @@ finalSections.push(
   jsonSections.data.body.map((x) => [...x, 'Yes', 'No', 'Linked'])
 )
 
-// export tsv
-try {
-  const tsv = json2tsv(finalSections.flat())
-  await writeFile(`${basename}${TSV_EXT}`, tsv, 'utf8')
-} catch (error) {
-  console.error(error)
-}
+await exportJsonArrayToTsv(filename, finalSections)
--- src/fs-helper.js
+++ src/fs-helper.js
@@ -1,4 +1,9 @@
-import { readFile } from 'node:fs/promises'
+import * as path from 'node:path'
+import { readFile, writeFile } from 'node:fs/promises'
+import { json2tsv } from 'tsv-json'
+
+const JSON_EXT = '.json'
+const TSV_EXT = '.tsv'
 
 export const getJsonFileContent = async (filename = '') => {
   try {
@@ -7,5 +12,15 @@ export const getJsonFileContent = async (filename = '') => {
   } catch (error) {
     console.error(error)
     throw new Error(error)
+  }
+}
+
+export const exportJsonArrayToTsv = async (filename = '', arr = []) => {
+  try {
+    const basename = path.basename(filename, JSON_EXT)
+    const tsv = json2tsv(arr.flat())
+    await writeFile(`${basename}${TSV_EXT}`, tsv, 'utf8')
+  } catch (error) {
+    console.error(error)
   }
 }
